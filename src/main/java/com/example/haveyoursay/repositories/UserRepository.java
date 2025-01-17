5e50a32ff6de1cafbe967487cdb69e17996b0133
Shane Dunne <77242413+shanedunne@users.noreply.github.com>
2025-01-17 17:24:29 +0000

    add user repositiory interface with mongo

---
+++ src/main/java/com/example/haveyoursay/repositories/UserRepository.java
@@ -0,0 +1,17 @@
+package com.example.haveyoursay.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.haveyoursay.models.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User,String> {
+    @Query("{email :?0}")
+    User findByEmail(String email);
+    
+
+} {
+    
+}
