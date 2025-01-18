c92df6e24490537abeb40d6a81aee83592425123
Shane Dunne <77242413+shanedunne@users.noreply.github.com>
2025-01-18 13:17:52 +0000

    add security config scripts

---
+++ src/main/java/com/example/haveyoursay/SecurityConfig/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.example.haveyoursay.SecurityConfig;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "zdtyozsS27cHKrrLU7frl0Owx++aZUQPTWC2xGXptv4=";
+    public static final String JWT_HEADER = "Authorization";
+}
