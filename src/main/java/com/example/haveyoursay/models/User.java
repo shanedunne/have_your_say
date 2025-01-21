ea3fd07c547e8f36d82eac3708789d3e69dfa780
Shane Dunne <77242413+shanedunne@users.noreply.github.com>
2025-01-21 19:18:12 +0000

    fix issue with env variables to get mongodb write working

--- src/main/java/com/example/haveyoursay/models/User.java
+++ src/main/java/com/example/haveyoursay/models/User.java
@@ -11,7 +11,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Document(collection = "user")
+@Document(collection = "users")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
