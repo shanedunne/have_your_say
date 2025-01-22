4ce3a55b9e20e5c8731933cc65c95d0f72e85999
Shane Dunne <77242413+shanedunne@users.noreply.github.com>
2025-01-22 14:02:57 +0000

    add petition model in backend

---
+++ src/main/java/com/example/haveyoursay/models/Petition.java
@@ -0,0 +1,81 @@
+package com.example.haveyoursay.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "petitions")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Petition {
+
+    @Id
+    private String id;
+    private String title;
+    private String category;
+    private String body;
+    private Integer startTime;
+    private Integer closeTime;
+    private String userId;
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Integer getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Integer startTime) {
+        this.startTime = startTime;
+    }
+
+    public Integer getCloseTime() {
+        return closeTime;
+    }
+
+    public void setCloseTime(Integer closeTime) {
+        this.closeTime = closeTime;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
