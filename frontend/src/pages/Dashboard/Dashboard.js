aac52e6435c085ee7d9995c3ac8e4a19e6ecec48
Shane Dunne <77242413+shanedunne@users.noreply.github.com>
2025-01-30 11:37:17 +0000

    add functionality to dynamically render menu based on user role

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from "react";
 import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import LayersIcon from '@mui/icons-material/Layers';
@@ -16,9 +16,85 @@ import OpenPetitions from '../OpenPetitions/OpenPetitions';
 import ClosedPetitions from '../ClosedPetitions/ClosedPetitions';
 import Logout from '../LogoutPage/LogoutPage';
 import logo from '../../assets/images/logo.png';
+import { useAuth } from '../../services/authProvider';
 
 
-export const NAVIGATION = [
+export const NAVIGATION_ADMIN = [
+    {
+        kind: 'header',
+        title: 'Create'
+    },
+    {
+        segment: 'createProposal',
+        title: 'Create Proposal',
+        icon: <CreateIcon />,
+    },
+    {
+        kind: 'divider',
+    },
+    {
+        kind: 'header',
+        title: 'Petitions',
+    },
+    {
+        segment: 'openPetitions',
+        title: 'Open Petitions',
+        icon: <FileOpenIcon />,
+    },
+    {
+        segment: 'closedPetitions',
+        title: 'Closed Petitions',
+        icon: <AssignmentTurnedInIcon />,
+    },
+    {
+        kind: 'divider',
+    },
+    {
+        kind: 'header',
+        title: 'Proposals',
+    },
+    {
+        segment: 'openProposals',
+        title: 'Open Proposals',
+        icon: <FileOpenIcon />,
+    },
+    {
+        segment: 'closedProposals',
+        title: 'Closed Proposals',
+        icon: <AssignmentTurnedInIcon />,
+    },
+    {
+        kind: 'divider',
+    },
+    {
+        kind: 'header',
+        title: 'Integrations',
+    },
+    {
+        segment: 'integrations',
+        title: 'Integrations',
+        icon: <LayersIcon />,
+    },
+    {
+        kind: 'divider',
+    },
+    {
+        kind: 'header',
+        title: 'Account',
+    },
+    {
+        segment: 'accountSettings',
+        title: 'Account Settings',
+        icon: <AccountCircleSharpIcon />,
+    },
+    {
+        segment: 'logout',
+        title: 'Logout',
+        icon: <LogoutSharpIcon />, 
+    },
+];
+
+export const NAVIGATION_CITIZEN = [
     {
         kind: 'header',
         title: 'Create'
@@ -94,7 +170,11 @@ export const NAVIGATION = [
 ];
 
 
+
+
 function PageContent({ pathname }) {
+
+    
     return (
         <Box
             sx={{
@@ -119,6 +199,14 @@ PageContent.propTypes = {
 
 function DashboardLayoutBasic(props) {
     const { window } = props;
+    let NAVIGATION;
+
+    const { role, community } = useAuth();
+    if (role === "ROLE_CITIZEN") {
+        NAVIGATION = NAVIGATION_CITIZEN;
+    } else if(role === "ROLE_ADMIN") {
+        NAVIGATION = NAVIGATION_ADMIN;
+    }
 
     const router = useDemoRouter('/dashboard');
 
